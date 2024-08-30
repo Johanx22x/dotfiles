abd75f85086a8e7bfe13c840e49f7a3f0f681238
Johanx22x <johanrodsa2210@gmail.com>
2024-08-29 22:46:39 -0600

    Add new xorg i3 theme

---
+++ xorg/evappuccin/config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# if type "xrandr"; then
+#   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+#     MONITOR=$m polybar --reload main &
+#   done
+# else
+#   polybar --reload main &
+# fi
+
+# Launch bar 
+polybar --reload main &
